8c4b47eee8fd6e83118425b0da833c04428b8341
Marvin Fette <fettema87760@th-nuernberg.de>
2022-11-12 10:34:29 +0100

    Hinzufügen einer Hilfsklasse um die Asset Uri aufzulösen

---
+++ OA Game/Assets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OA_Game
+{
+    /// <summary>
+    /// Helper class to handle the assets
+    /// </summary>
+    public static class Assets
+    {
+        /// <summary>
+        /// Helper method to get a uri object of a given absolute asset path
+        /// </summary>
+        /// <param name="asset">Absolute assets path</param>
+        /// <returns>New uri object with the correct schema</returns>
+        public static Uri GetUri(string asset) => new Uri("pack://siteoforigin:,,," + asset);
+    }
+}
