103fb4d24e8633ed66b4d0a6995e0078a0e11dce
andreas.neumueller <andreas.neumueller@siemens.com>
2022-11-14 17:57:09 +0100

    Implement Class Architecture

---
+++ OA Game/DrawableObjects/Items/Items.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using GameEngine.GameObjects;
+
+namespace OA_Game.DrawableObjects
+{
+    public class Items : DrawableObject
+    {
+        public Items(Rectangle mapObject) : base(mapObject)
+        {
+        }
+    }
+}
---
+++ OA Game/DrawableObjects/Obstacles/Obstacles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using GameEngine.GameObjects;
+
+namespace OA_Game.DrawableObjects.Obstacles
+{
+    public class Obstacles : DrawableObject
+    {
+        public Obstacles(Rectangle mapObject) : base(mapObject)
+        {
+        }
+    }
+}
