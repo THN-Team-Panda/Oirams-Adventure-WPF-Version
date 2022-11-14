6ef5d966814839662c39dad7f26327f4ba15c251
Edward Simonjan <simonjaned87761@th-nuernberg.de>
2022-11-14 12:52:38 +0100

    - Kommentare auf englisch geändert - Save Methode durch Array struktierter gemacht - noch nicht ganz fertig

--- GameEngine/Saving.cs
+++ GameEngine/Saving.cs
@@ -9,46 +9,85 @@ namespace GameEngine
 {
     public class Saving
     {
-
+        /// <summary>
+        /// a path, that is being given by the user when creating a Saving object; 
+        /// private because it is only needed within this class
+        /// </summary>
         private string path;
+
+        /// <summary>
+        /// a constructor that requires the user to add a path when creating a saving object
+        /// </summary>
+        /// <param name="path"></param>
         public Saving(string path)
         {
             this.path = path;
         }
 
-        public static async Task Save(int level, string path)
-        {
-            StreamWriter stw = new StreamWriter(path);
-            string savingvar = "0";
 
-            for (int j = 0; j <= level; j++) // j entspricht Koordinaten der Level (zeile 1 = level 1 usw.)
+        /// <summary>
+        /// a function that saves the current game progress in a data file;
+        /// it needs a level as parameter in order to know which level so save
+        /// </summary>
+        /// <param name="level"></param>
+        public void Save(int level)
+        {
+            //erst öffnen StreamReader dann StreamWriter
+            //und Exceptions für versch. Fälle machen
+
+            StreamReader str = new StreamReader(path);
+
+            while (!str.EndOfStream)
+            {
+                string line = str.ReadLine();
+
+            }
+            str.Close();
+
+            StreamWriter stw = new StreamWriter(path);
+            string[] coordinate = new string[4];
+
+            for (int j = 0; j < coordinate.Length; j++) // j correlates to the coordinates of the levels (i.e. line 1 = level 1 etc.)
             {
 
                 if (j != level)
                 {
-                    savingvar = "0";
-                    stw.WriteLine(savingvar);
-                    // diese Methode schreibt die Savingsvariable in die Datei des angegeben Pfades
+                    coordinate[j] = "0";
+
                 }
 
-                else if (j == level) // && j.data == 1 => dafür Array-Ansatz
+                if (j == level) // && j.data == 1 => dafür Array-Ansatz (auch für AlreadySaved-Funktion)
                 {
-                    savingvar = "1";
-                    stw.WriteLine(savingvar);
-                    //await File.WriteAllTextAsync(path, savingvar);
-                    // diese Methode schreibt die Savingsvariable in die Datei des angegeben Pfades
+                    coordinate[j] = "1";
+
+                    //note for myself: await File.WriteAllTextAsync(path, savingvar);
                 }
 
 
             }
+
+            for (int i = 0; i < coordinate.Length; i++)
+            {
+                stw.Write(coordinate[i]);
+            }
+
+
+
             stw.Close();
 
 
         }
 
-        public static void AlreadySaved(int level, string path) //gibt dem Benutzer auf Anfrage aus, ob das im Parameter angegebene Level bereits gespeichert wurde
+        /// <summary>
+        /// this function checks on request wether a level has already been saved or not 
+        /// </summary>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        public bool AlreadySaved(int level)
         {
+            //eventuelle Änderung: statt jedes Level anzuzeigen, nur den Speicherfortschritt des angefragten Levels anzeigen
             StreamReader str = new StreamReader(path);
+            bool issaved = false;
 
             while (!str.EndOfStream)
             {
@@ -56,15 +95,17 @@ namespace GameEngine
 
                 if (line == "1")
                 {
-                    Console.WriteLine("Level wurde bereits gespeichert!");
+                    issaved = true;
                 }
                 else if (line == "0")
                 {
-                    Console.WriteLine("Noch kein Spielfortschritt in diesem Level vorhanden!");
+                    issaved = false;
                 }
             }
 
             str.Close();
+
+            return issaved;
         }
 
     }
