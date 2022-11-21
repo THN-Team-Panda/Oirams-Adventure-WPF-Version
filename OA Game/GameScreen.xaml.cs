1b1d9a5b8443d35a85b0d74bbd284486f2929380
gatzkevi89203 <gatzkevi89203@th-nuernberg.de>
2022-11-21 09:30:04 +0100

    Load Map Image fertig

--- OA Game/GameScreen.xaml.cs
+++ OA Game/GameScreen.xaml.cs
@@ -33,55 +33,35 @@ namespace OA_Game
             this.level_id = level_id;
             
 
-            map.Children.Add(Map_Generator.Map_Loading(level_id));
-            Canvas.SetTop(Map_Generator.Map_Loading(level_id), 0);
-            Canvas.SetLeft(Map_Generator.Map_Loading(level_id), 0);
-
-            //map.Background = Map_Generator.Set_CanvasBackground(1);
-
-
-        }
-
-
-    }
-
-    public class Map_Generator 
-    {
-
-        public int level_id = 1;
-
-        public Map_Generator(int level_id)
-        {
-            this.level_id = level_id;
-            Map_Loading(level_id);
-        }
-            
-
-        public static Image Map_Loading(int level_id)
-        {
-            string file_name = $"Level" + level_id + ".tmx";
+            string file_name = $"Level" + 1 + ".tmx";
 
             string mapdirectory = Assets.GetPath("Level_Panda");
-            
-            Map level = new Map(file_name, mapdirectory, new int[] { 2 }, new int[] { 3 }, new int[] { 4 });
+
+            Map level = new Map(file_name, mapdirectory, new int[] {1,2,3,4,13,14,15,26,27,28 }, new int[] {84,78,91 }, new int[] {104,105,106,107,108,109,117,118,76});
 
             Image MapImage = level.RenderTiles();
 
-            return MapImage;
+            map.Children.Add(MapImage);
+            Canvas.SetTop(MapImage, 0);
+            Canvas.SetLeft(MapImage, 0);
+           
 
 
         }
 
-        public static ImageBrush Set_CanvasBackground(int level_id)
+        public static void Set_CanvasBackground(int level_id)
         {
             ImageBrush background = new ImageBrush();
             string file_name = "Background" + level_id;
             background.ImageSource = new BitmapImage(Assets.GetUri("Platzhalter"));
-            return background;
+            
 
         }
 
+
     }
+
+    
     
     
 }
