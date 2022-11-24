ec17816cf1b40b265ac5b22670cbd704227ca739
Marvin Fette <fettemarvin@gmail.com>
2022-11-24 12:30:49 +0100

    Hinzufügen zweier Enums die alle Tile Ids umfassen

---
+++ OA Game/MapCollections/GroundTiles.cs
@@ -0,0 +1,128 @@
+﻿namespace OA_Game.MapCollections
+{
+    /// <summary>
+    /// Ids of the ground tiles from the level deisng
+    /// </summary>
+    public enum GroundTiles
+    {
+        // Dark Blocks
+        Dark1 = 47,
+        Dark2 = 48,
+        Dark3 = 58,
+
+        // Gras blocks
+        Gras1 = 0,
+        Gras2 = 1,
+        Gras3 = 2,
+        Gras4 = 81,
+        Gras5 = 82,
+        Gras6 = 83,
+
+        // Wood
+        Wood1 = 52,
+        Wood2 = 53,
+
+        // Ladder
+        ladder = 45,
+
+        // Lamp
+        lamp = 84,
+
+        // Leaf
+        Leaf1 = 40,
+        Leaf2 = 41,
+
+        // Dirt blocks
+        Dirt1 = 13,
+        Dirt2 = 14,
+        Dirt3 = 15,
+        Dirt4 = 26,
+        Dirt5 = 27,
+        Dirt6 = 28,
+        Dirt7 = 3,
+        Dirt8 = 16,
+        Dirt9 = 29,
+        Dirt10 = 6,
+        Dirt11 = 19,
+        Dirt12 = 32,
+        
+        DirtHalfStep1 = 4,
+        DirtHalfStep2 = 5,
+        DirtHalfStep3 = 17,
+        DirtHalfStep4 = 18,
+        DirtHalfStep5 = 30,
+        DirtHalfStep6 = 31
+
+
+        // Brick tiles
+        BrickGroupLarge1 = 42,
+        BrickGroupLarge2 = 43,
+        BrickGroupLarge3 = 44,
+        BrickGroupLarge4 = 55,
+        BrickGroupLarge5 = 56,
+        BrickGroupLarge6 = 57,
+        BrickGroupLarge7 = 68,
+        BrickGroupLarge8 = 69,
+        BrickGroupLarge9 = 70,
+        BrickGroupSmall10 = 50,
+        BrickGroupSmall11 = 51,
+        BrickGroupSmall12 = 63,
+        BrickGroupSmall13 = 64,
+        BrickGroupVertical1 = 7,
+        BrickGroupVertical2 = 8,
+        BrickGroupVertical3 = 9,
+        BrickGroupVertical4 = 10,
+        BrickGroupVertical5 = 21,
+        BrickGroupVertical6 = 22,
+        BrickGroupVertical7 = 33,
+        BrickGroupVertical8 = 34,
+        BrickGroupVertical9 = 35,
+        BrickGroupVertical10 = 36,
+        BrickGroupVertical11 = 46,
+        BrickGroupVertical14 = 49,
+        BrickSolo = 93,
+
+
+        // Dark bricks
+        DarkBrickGroup1 = 11,
+        DarkBrickGroup2 = 12,
+        DarkBrickGroup3 = 24,
+        DarkBrickGroup4 = 25,
+        DarkBrickGroup5 = 37,
+        DarkBrickGroup6 = 38,
+
+        // Dungeon Blocks
+        DungeonGroup1 = 65,
+        DungeonGroup2 = 66,
+        DungeonGroup3 = 67,
+        DungeonGroup4 = 78,
+        DungeonGroup5 = 79,
+        DungeonGroup6 = 80,
+        DungeonGroup7 = 91,
+        DungeonGroup8 = 92,
+        DungeonGroup9 = 54,
+
+        // Dungeon Dark Bricks
+        DungeonDarkBrickGroup1 = 60,
+        DungeonDarkBrickGroup2 = 61,
+        DungeonDarkBrickGroup3 = 62,
+        DungeonDarkBrickGroup20 = 71,
+        DungeonDarkBrickGroup4 = 72,
+        DungeonDarkBrickGroup5 = 73,
+        DungeonDarkBrickGroup6 = 74,
+        DungeonDarkBrickGroup7 = 75,
+        DungeonDarkBrickGroup8 = 85,
+        DungeonDarkBrickGroup9 = 87,
+        DungeonDarkBrickGroup10 = 88,
+        DungeonDarkBrickGroup11 = 89,
+        DungeonDarkBrickGroup12 = 94,
+        DungeonDarkBrickGroup21 = 95,
+        DungeonDarkBrickGroup13 = 96,
+        DungeonDarkBrickGroup14 = 97,
+        DungeonDarkBrickGroup15 = 98,
+        DungeonDarkBrickGroup16 = 99,
+        DungeonDarkBrickGroup17 = 100,
+        DungeonDarkBrickGroup18 = 101,
+        DungeonDarkBrickGroup19 = 102
+    }
+}
---
+++ OA Game/MapCollections/ObstacleTiles.cs
@@ -0,0 +1,23 @@
+﻿namespace OA_Game.MapCollections
+{
+    /// <summary>
+    /// Ids of the obstacle tiles from the level deisng
+    /// </summary>
+    public enum ObstacleTiles
+    {
+        // Spikes
+        Spikes = 76,
+
+        // Cactus
+        Cactus1 = 106,
+        Cactus2 = 107,
+        Cactus3 = 108,
+        Cactus4 = 109,
+
+        // Water
+        Water1 = 104,
+        Water2 = 105,
+        Water3 = 117,
+        Water4 = 118
+    }
+}
