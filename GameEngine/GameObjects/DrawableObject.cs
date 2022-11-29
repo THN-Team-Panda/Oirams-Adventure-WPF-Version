50fe905415c24b31cd1f8da8bf4333882f511d09
gatzkevi89203 <gatzkevi89203@th-nuernberg.de>
2022-11-29 21:37:42 +0100

    Note Spitessequenz erstellen und Spritessequenzen in DrawableObject packen

--- GameEngine/GameObjects/DrawableObject.cs
+++ GameEngine/GameObjects/DrawableObject.cs
@@ -2,6 +2,7 @@
 using System.Windows.Controls;
 using System.Windows;
 using System.Windows.Media;
+using GameEngine.Exceptions;
 
 namespace GameEngine.GameObjects
 {
@@ -94,6 +95,18 @@ namespace GameEngine.GameObjects
         }
 
         /// <summary>
+        /// A collection of playable sequences to animate drawable objects
+        /// </summary>
+        protected Dictionary<string, PlayableSequence> AnimationCollection = new();
+
+
+
+        /// <summary>
+        /// Default sprite
+        /// </summary>
+        protected ImageSource defaultSprite;
+
+        /// <summary>
         /// Set a sprite as Image brush
         /// NOTE: Default is the direction right
         /// </summary>
@@ -113,6 +126,84 @@ namespace GameEngine.GameObjects
             }
 
             Rectangle.Fill = brush;
+        }
+
+        /// <summary>
+        /// Add a playable sequence to the object
+        /// </summary>
+        /// <param name="name">Identifier of the sequence</param>
+        /// <param name="sequence">Sequence object</param>
+        public void AddSequence(string name, PlayableSequence sequence) => AnimationCollection.Add(name, sequence);
+
+        /// <summary>
+        /// Remove a playable sequence from the object
+        /// </summary>
+        /// <param name="name">Identifier of the sequence</param>
+        /// <exception cref="UnknownAnimationSequenceException">If the sequence name is unkown</exception>
+        public void RemoveSequence(string name)
+        {
+            if (!AnimationCollection.ContainsKey(name))
+                throw new UnknownAnimationSequenceException();
+
+            AnimationCollection.Remove(name);
+        }
+
+        /// <summary>
+        /// Play a sequence one time
+        /// </summary>
+        /// <param name="name">Identifier of the sequence</param>
+        /// <param name="directionLeft"></param>
+        /// <param name="lastIsDefault"></param>
+        /// <exception cref="UnknownAnimationSequenceException">If the sequence name is unkown</exception>
+        public void PlaySequence(string name, bool directionLeft = false, bool lastIsDefault = false)
+        {
+            if (!AnimationCollection.ContainsKey(name))
+                throw new UnknownAnimationSequenceException();
+
+            PlayableSequence sequence = AnimationCollection[name];
+
+            foreach (ImageSource image in sequence)
+            {
+                SetSprite(image, directionLeft);
+
+                Thread.Sleep(sequence.Between);
+            }
+
+            if (lastIsDefault)
+                SetSprite(defaultSprite, directionLeft);
+
+        }
+
+        /// <summary>
+        /// Play a sequence one time async in the background
+        /// </summary>
+        /// <param name="name">Identifier of the sequence</param>
+        /// <param name="directionLeft"></param>
+        /// <param name="lastIsDefault"></param>
+        /// <exception cref="UnknownAnimationSequenceException">If the sequence name is unkown</exception>
+        public async void PlaySequenceAsync(string name, bool directionLeft = false, bool lastIsDefault = false)
+        {
+            if (playToken)
+                return;
+
+            playToken = true;
+
+            if (!AnimationCollection.ContainsKey(name))
+                throw new UnknownAnimationSequenceException();
+
+            PlayableSequence sequence = AnimationCollection[name];
+
+            foreach (ImageSource image in sequence)
+            {
+                SetSprite(image, directionLeft);
+
+                await Task.Delay(sequence.Between);
+            }
+
+            if (lastIsDefault)
+                SetSprite(defaultSprite, directionLeft);
+
+            playToken = false;
         }
     }
 }
--- OA Game/DrawableObjects/Items/Note.cs
+++ OA Game/DrawableObjects/Items/Note.cs
@@ -1,12 +1,26 @@
-﻿namespace OA_Game.Items
+﻿using GameEngine;
+using GameEngine.GameObjects;
+using System;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+
+namespace OA_Game.Items
 {
     /// <summary>
     /// Class for item Note. Is the munition to shoot for the player.
     /// </summary>
     public class Note : Items
     {
-        public Note(int height, int width) : base(height, width)
+        public Note(int height, int width, ImageSource deafultSprite) : base(height, width)
         {
+            PlayableSequence animation = new PlayableSequence(new ImageSource[]
+            {
+                new BitmapImage(Assets.GetUri(""))
+            });
+            
         }
+        
+
     }
 }
