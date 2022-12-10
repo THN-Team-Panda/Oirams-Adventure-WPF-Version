c030cdfb330fb628ad90f2222cf8cbdb2e7e2479
gatzkevi89203 <gatzkevi89203@th-nuernberg.de>
2022-12-10 15:10:34 +0100

    implement basics for konkeydong and create new class boombox

---
+++ OA Game/AnimatedObjects/Enemies/Boombox.cs
@@ -0,0 +1,42 @@
+﻿using GameEngine;
+using OA_Game.Enemies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace OA_Game.AnimatedObjects.Enemies
+{
+    internal class Boombox : Enemy
+    {
+        /// <summary>
+        /// property to check the damage output
+        /// </summary>
+        public override int Damage { get; } = 1;
+
+        public Boombox(int height, int width, ImageSource defaultSprite) : base(height, width, defaultSprite)
+        {
+            PlayableSequence boombox = new PlayableSequence(new ImageSource[]
+            {
+                new BitmapImage(Assets.GetUri("Images/Cap/Cap_1.png")),
+                new BitmapImage(Assets.GetUri("Images/Cap/Cap_2.png")),
+                new BitmapImage(Assets.GetUri("Images/Cap/Cap_3.png")),
+                new BitmapImage(Assets.GetUri("Images/Cap/Cap_4.png")),
+                new BitmapImage(Assets.GetUri("Images/Cap/Cap_3.png")),
+                new BitmapImage(Assets.GetUri("Images/Cap/Cap_2.png")),
+                new BitmapImage(Assets.GetUri("Images/Cap/Cap_5.png")),
+                new BitmapImage(Assets.GetUri("Images/Cap/Cap_6.png")),
+                new BitmapImage(Assets.GetUri("Images/Cap/Cap_7.png")),
+                new BitmapImage(Assets.GetUri("Images/Cap/Cap_6.png")),
+                new BitmapImage(Assets.GetUri("Images/Cap/Cap_5.png"))
+            });
+            boombox.Between = TimeSpan.FromMilliseconds(150);
+            AddSequence("animation_boombox", boombox);
+
+            EndlessLoopSequenceAsync("animation_boombox", true);
+        }
+    }
+}
--- OA Game/AnimatedObjects/Enemies/KonkeyDong.cs
+++ OA Game/AnimatedObjects/Enemies/KonkeyDong.cs
@@ -1,6 +1,8 @@
 ﻿using GameEngine;
 using GameEngine.GameObjects;
+using System;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace OA_Game.Enemies
 {
@@ -20,12 +22,71 @@ namespace OA_Game.Enemies
 
         public KonkeyDong(int height, int width, ImageSource defaultSprite) : base(height, width, defaultSprite)
         {
-            // TODO
+            DirectionLeft = true;
+            PlayableSequence donkeykongMove = new PlayableSequence(new ImageSource[]
+            {
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Movement/Skeleton_Movement_1.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Movement/Skeleton_Movement_2.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Movement/Skeleton_Movement_3.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Movement/Skeleton_Movement_4.png"))
+            });
+            donkeykongMove.Between = TimeSpan.FromMilliseconds(150);
+            this.AddSequence("move_donkeykong", donkeykongMove);
+
+            PlayableSequence konkeydongAttack = new PlayableSequence(new ImageSource[]
+            {
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Attack/Skeleton_Attacke_1.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Attack/Skeleton_Attacke_2.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Attack/Skeleton_Attacke_3.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Attack/Skeleton_Attacke_4.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Attack/Skeleton_Attacke_5.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Attack/Skeleton_Attacke_6.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Attack/Skeleton_Attacke_7.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Attack/Skeleton_Attacke_8.png"))
+            });
+            konkeydongAttack.Between = TimeSpan.FromMilliseconds(150);
+            this.AddSequence("attack_konkeydong", konkeydongAttack);
+
+            PlayableSequence konkeydongDying = new PlayableSequence(new ImageSource[]
+            {
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Dying/Skeleton_Dying_1.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Dying/Skeleton_Dying_2.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Dying/Skeleton_Dying_3.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Dying/Skeleton_Dying_4.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Dying/Skeleton_Dying_5.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Dying/Skeleton_Dying_6.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Dying/Skeleton_Dying_7.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Dying/Skeleton_Dying_8.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Dying/Skeleton_Dying_9.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Dying/Skeleton_Dying_10.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Dying/Skeleton_Dying_11.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Dying/Skeleton_Dying_12.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Dying/Skeleton_Dying_13.png")),
+                new BitmapImage(Assets.GetUri("Images/Skeleton/Dying/Skeleton_Dying_14.png"))
+
+            });
+            konkeydongDying.SequenceFinished += (object sender) => { ObjectIsTrash = true; };
+            konkeydongDying.Between = TimeSpan.FromMilliseconds(150);
+            this.AddSequence("dying_konkeydong", konkeydongDying);
+            Spawn_Boombox();
+        }
+        /// <summary>
+        /// spawn boombox (enemy)
+        /// </summary>
+        public void Spawn_Boombox()
+        {
+
         }
 
         public void Attack(AnimatedObject obj)
         {
-            // TODO
+            if (IsDying)
+                return;
+
+            if (obj is Player player)
+                player.GetDamage(Damage);
+
+            PlaySequenceAsync("attack_konkeydong", DirectionLeft, true, true);
         }
 
         public void Move(Map map)
@@ -35,12 +96,17 @@ namespace OA_Game.Enemies
 
         public void GetDamage(int damage)
         {
-            // TODO
+            if (IsDying)
+                return;
+
+            if (damage > 0)
+                Die();
         }
 
         public void Die()
         {
-            //TODO
+            IsDying = true;
+            PlaySequenceAsync("dying_konkeydong", DirectionLeft, false, true);
         }
     }
 }
