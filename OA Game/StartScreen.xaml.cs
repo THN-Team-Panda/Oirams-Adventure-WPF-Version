4417f51bdfef0e4a99544747b1d25bb17b292576
t2brozz <42860763+T2brozz@users.noreply.github.com>
2022-12-13 00:12:30 +0100

    Hinzufügen der Buttongeneration

--- OA Game/StartScreen.xaml.cs
+++ OA Game/StartScreen.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using GameEngine;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +15,10 @@ namespace OA_Game
     /// </summary>
     public partial class StartScreen
     {
+        /// <summary>
+        /// File where GameSave is located
+        /// </summary>
+        private Saving save;
 
         public StartScreen()
         {
@@ -21,35 +28,135 @@ namespace OA_Game
             LevelView.Width = Preferences.ViewWidth;
 
             Backscreen.Source = new BitmapImage(Assets.GetUri("Images/Environment/back.png"));
+            save = new(Preferences.GameDataPath);
 
-            Level1.Background = new ImageBrush(new BitmapImage(Assets.GetUri("Images/StartScreen/Buttons/button_background_1.jpg")));
-            Level2.Background = new ImageBrush(new BitmapImage(Assets.GetUri("Images/StartScreen/Buttons/button_background_2.jpg")));
-            Level3.Background = new ImageBrush(new BitmapImage(Assets.GetUri("Images/StartScreen/Buttons/button_background_3.jpg")));
 
-            EnableButton();
+            CreateLevelButtons();
         }
+
+
+        /// <summary>
+        /// Generate Buttons 
+        /// </summary>
+        private void CreateLevelButtons()
+        {
+            // Clear all existing Buttons
+            Levels.Children.RemoveRange(0, Levels.Children.Count);
+
+            //read background files
+            List<string> backgroundFiles =
+                Directory.EnumerateFiles(Assets.GetPath("Images/StartScreen/Buttons"), "*.jpg").ToList();
+            //read File names in Level Panda Directory 
+            List<string> levelFiles = Directory.EnumerateFiles(Assets.GetPath("Level_Panda"), "*.tmx").ToList();
+            for (int i = 0; i < levelFiles.Count; i++) // remove Path from filename
+            {
+                levelFiles[i] = Path.GetFileName(levelFiles[i]);
+            }
+
+            //get bitmap images
+            BitmapImage[] brushes = new BitmapImage[backgroundFiles.Count];
+            for (int i = 0; i < brushes.Length; i++)
+            {
+                brushes[i] = new BitmapImage(new Uri(backgroundFiles[i]));
+            }
+
+            //read highscores of level
+            TimeSpan[] levelTimes = save.ReadLevels();
+            for (int i = 0; i < levelFiles.Count; i++)
+            {
+                //check if level
+                TimeSpan levelTime;
+                try
+                {
+                    levelTime = levelTimes[i];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    levelTime = TimeSpan.Zero;
+                }
+
+                string levelName = levelFiles[i][..^4];
+                //create button
+                Button btn = new()
+                {
+                    Name = $"{levelName}",
+                    Style = Resources["MenuButtons"] as Style,
+                    Tag = (i + 1),
+                    IsEnabled = (!(levelTime == TimeSpan.Zero) || i == levelTimes.Length),
+                };
+                //create stackpanel inside btn
+                StackPanel btnStackPanel = new()
+                {
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Background = btn.IsEnabled switch
+                    {
+                        false => new ImageBrush(new FormatConvertedBitmap(brushes[Math.Clamp(i, 0, brushes.Length)],
+                            PixelFormats.Gray4, null, 0)),
+                        true => new ImageBrush(brushes[Math.Clamp(i, 0, brushes.Length)]),
+                    },
+                    Width = btn.Width
+                };
+                //add level name label
+                btnStackPanel.Children.Add(new Label()
+                {
+                    Margin = new Thickness(0, -10, 0, 0),
+                    Content = $"{levelName}",
+                    HorizontalContentAlignment = HorizontalAlignment.Center,
+                    Foreground = btn.IsEnabled == false ? new SolidColorBrush(Colors.Black) : btn.Foreground
+                });
+                //add level highscore label
+                btnStackPanel.Children.Add(new Label()
+                {
+                    Margin = new Thickness(0, -13, 0, 0),
+                    Content = $"{levelTime:g}",
+                    FontSize = 12, HorizontalContentAlignment = HorizontalAlignment.Center,
+                    Foreground = btn.IsEnabled == false ? new SolidColorBrush(Colors.Black) : btn.Foreground
+                });
+
+                btn.Content = btnStackPanel;
+                //subscribe events
+                btn.Click += OpenLevel_Click;
+                btn.MouseEnter += Btn_GotFocus;
+                btn.MouseLeave += Btn_MouseLeave;
+                //add level
+                Levels.Children.Add(btn);
+            }
+        }
+
+        /// <summary>
+        /// Function that gets triggered when mouse is leaving button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            ((Button)sender).Opacity = 1;
+        }
+
+        /// <summary>
+        /// Function that is triggered when mouse hovers over button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ((Button)sender).Opacity = 0.75;
+        }
+
         /// <summary>
         /// EventHandler for click button. If you click the button the methode OpenLevel is called and gets the level as int.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         /// <exception cref="ArgumentOutOfRangeException"></exception>
-
         private void OpenLevel_Click(object sender, RoutedEventArgs e)
         {
-
             Button senderButton = (Button)sender;
-            OpenLevel(senderButton.Content switch
-            {
-                "Level 1" => 1,
-                "Level 2" => 2,
-                "Level 3" => 3,
-                _ => throw new ArgumentOutOfRangeException(nameof(sender), "You didn't click on a Button HOW??")
-            });
-
+            OpenLevel((int)senderButton.Tag);
         }
+
         /// <summary>
-        /// Call the constructer if Gamescreen to start the level and hide the Startscreen.
+        /// Call the constructor if Gamescreen to start the level and hide the Startscreen.
         /// </summary>
         /// <param name="clickLevel"></param>
         private void OpenLevel(int clickLevel)
@@ -65,32 +172,9 @@ namespace OA_Game
 
             gameScreen.ShowDialog(); //show the game screen; dialog is a popup-window
 
-            EnableButton();
+            CreateLevelButtons();
 
             Show();
         }
-        /// <summary>
-        /// If the prelevel was finished succesfully the next level is possible to play and the button gets enabled.
-        /// </summary>
-        private void EnableButton()
-        {
-            Level1.IsEnabled = true;
-            Level2.IsEnabled = false;
-            Level3.IsEnabled = false;
-
-            Saving saving = new(Preferences.GameDataPath);
-
-
-            if (saving.AlreadySaved(1)) // Level 2 will be available to play after Level 1 has been saved (after player has finished level 1)
-            {
-                Level2.IsEnabled = true;
-            }
-
-            if (saving.AlreadySaved(2)) // Level 3 will be available to play after Level 2 has been saved (after player has finished level 2)
-            {
-                Level3.IsEnabled = true;
-            }
-        }
-
     }
 }
